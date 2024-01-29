b06a668cae4b8aec8f89043ede6eaeb9b3a6dd58
Iliailineya <ilavituk011@gmail.com>
2024-01-29 22:11:27 +0200

    Fix wildcard import / add @BeforeAll setup()

--- src/test/java/service/AccountServiceTest.java
+++ src/test/java/service/AccountServiceTest.java
@@ -2,6 +2,7 @@ package service;
 
 import model.Account;
 import model.enams.Gender;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -11,47 +12,48 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class AccountServiceTest {
 
+    private static AccountService accountService;
+    private static List<Account> accounts;
+
+    @BeforeAll
+    static void setUp() {
+        accountService = new AccountService();
+        accounts = createTestAccounts();
+    }
     @Test
     void testFilterAccountsByBalance() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         List<Account> result = accountService.filterAccountsByBalance(accounts);
-        assertEquals(4, result.size());
         assertNotNull(result);
+        assertEquals(4, result.size());
     }
 
     @Test
     void testGetUniqueCountries() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         Set<String> result = accountService.getUniqueCountries(accounts);
-        assertEquals(4, result.size());
         assertNotNull(result);
+        assertEquals(4, result.size());
     }
 
     @Test
     void testHasYoungerThan2000() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         assertTrue(accountService.hasYoungerThan2000(accounts));
     }
 
     @Test
     void testGetBalanceOfMaleAccounts() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         double result = accountService.getBalanceOfMaleAccounts(accounts);
         assertEquals(29000d, result);
     }
 
     @Test
     void testGroupByBirthMonth() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         Map<Integer, List<Account>> result = accountService.groupByBirthMonth(accounts);
         assertNotNull(result);
         assertEquals(4, result.size());
@@ -59,16 +61,12 @@ class AccountServiceTest {
 
     @Test
     void testGetAverageBalanceByCountry() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         double result = accountService.getAverageBalanceByCountry(accounts, "USA");
         assertEquals(16000.0, result);
     }
 
     @Test
     void testGetNamesConcatenated() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         String result = accountService.getNamesConcatenated(List.of(accounts));
         assertNotNull(result);
         assertEquals("John Doe, Alice Johnson, Bob Smith, Emily Clark", result);
@@ -76,8 +74,6 @@ class AccountServiceTest {
 
     @Test
     void testGetSortedAccounts() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         List<Account> sortedAccounts = accountService.getSortedAccounts(accounts);
         assertNotNull(sortedAccounts);
         assertEquals("Clark", sortedAccounts.get(0).getLastName());
@@ -85,16 +81,12 @@ class AccountServiceTest {
 
     @Test
     void testGetOldestAccount() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         Account oldestAccount = accountService.getOldestAccount(accounts);
         assertEquals("John", oldestAccount.getFirstName());
     }
 
     @Test
     void testGroupByBirthYearAndAverageBalance() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         Map<Integer, Double> result = accountService.groupByBirthYearAndAverageBalance(accounts);
         assertNotNull(result);
         assertEquals(4, result.size());
@@ -102,13 +94,11 @@ class AccountServiceTest {
 
     @Test
     void testGetAccountWithLongestLastName() {
-        List<Account> accounts = createTestAccounts();
-        AccountService accountService = new AccountService();
         Account accountWithLongestLastName = accountService.getAccountWithLongestLastName(accounts);
         assertEquals("Johnson", accountWithLongestLastName.getLastName());
     }
 
-    private List<Account> createTestAccounts() {
+    private static List<Account> createTestAccounts() {
         return Arrays.asList(
                 new Account("John", "Doe", "USA", LocalDate.of(1990, Month.JANUARY, 12), 16000d, Gender.MALE),
                 new Account("Alice", "Johnson", "Canada", LocalDate.of(1995, Month.MARCH, 5), 18000d, Gender.FEMALE),
