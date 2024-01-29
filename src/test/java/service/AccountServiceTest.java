c9fb7476f328ed8bc46f7be2d9eb01f75a012928
Iliailineya <ilavituk011@gmail.com>
2024-01-29 17:23:52 +0200

    AccountService created

---
+++ src/test/java/service/AccountServiceTest.java
@@ -0,0 +1,48 @@
+package service;
+
+class AccountServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void filterAccountsByBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUniqueCountries() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasYoungerThan2000() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalanceOfMaleAccounts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void groupByBirthMonth() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAverageBalanceByCountry() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNamesConcatenated() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSortedAccounts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getOldestAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void groupByBirthYearAndAverageBalance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAccountWithLongestLastName() {
+    }
+}
