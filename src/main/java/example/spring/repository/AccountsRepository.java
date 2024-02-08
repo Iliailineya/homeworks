68d81bea9c3e2d1034cfe68b1da534f9422e078e
Iliailineya <ilavituk011@gmail.com>
2024-02-08 13:20:35 +0200

    rename accountsRepository toAccountsRepository, rename AccountService to AccountServiceForFun

--- src/main/java/example/spring/repository/AccountsRepository.java
+++ src/main/java/example/spring/repository/AccountsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface accountsRepository extends CrudRepository<Account, Integer> {
+public interface AccountsRepository extends CrudRepository<Account, Integer> {
 }
--- src/test/java/service/AccountServiceForFunTest.java
+++ src/test/java/service/AccountServiceForFunTest.java
@@ -2,7 +2,7 @@ package service;
 
 import example.spring.model.Account;
 import example.spring.model.enams.Gender;
-import example.spring.service.AccountService;
+import example.spring.service.AccountServiceForFun;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -18,93 +18,93 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-class AccountServiceTest {
+class AccountServiceForFunTest {
 
-    private static AccountService accountService;
+    private static AccountServiceForFun accountServiceForFun;
     private static List<Account> accounts;
 
     @BeforeAll
     static void setUp() {
-        accountService = new AccountService();
+        accountServiceForFun = new AccountServiceForFun();
         accounts = createTestAccounts();
     }
     @Test
     void testFilterAccountsByBalance() {
-        List<Account> result = accountService.filterAccountsByBalance(accounts);
+        List<Account> result = accountServiceForFun.filterAccountsByBalance(accounts);
         assertNotNull(result);
         assertEquals(4, result.size());
     }
 
     @Test
     void testGetUniqueCountries() {
-        Set<String> result = accountService.getUniqueCountries(accounts);
+        Set<String> result = accountServiceForFun.getUniqueCountries(accounts);
         assertNotNull(result);
         assertEquals(4, result.size());
     }
 
     @Test
     void testHasYoungerThan2000() {
-        assertTrue(accountService.hasYoungerThan2000(accounts));
+        assertTrue(accountServiceForFun.hasYoungerThan2000(accounts));
     }
 
     @Test
     void testGetBalanceOfMaleAccounts() {
-        double result = accountService.getBalanceOfMaleAccounts(accounts);
+        double result = accountServiceForFun.getBalanceOfMaleAccounts(accounts);
         assertEquals(29000d, result);
     }
 
     @Test
     void testGroupByBirthMonth() {
-        Map<Integer, List<Account>> result = accountService.groupByBirthMonth(accounts);
+        Map<Integer, List<Account>> result = accountServiceForFun.groupByBirthMonth(accounts);
         assertNotNull(result);
         assertEquals(4, result.size());
     }
 
     @Test
     void testGetAverageBalanceByCountry() {
-        double result = accountService.getAverageBalanceByCountry(accounts, "USA");
+        double result = accountServiceForFun.getAverageBalanceByCountry(accounts, "USA");
         assertEquals(16000.0, result);
     }
 
     @Test
     void testGetNamesConcatenated() {
-        String result = accountService.getNamesConcatenated(List.of(accounts));
+        String result = accountServiceForFun.getNamesConcatenated(List.of(accounts));
         assertNotNull(result);
         assertEquals("John Doe, Alice Johnson, Bob Smith, Emily Clark", result);
     }
 
     @Test
     void testGetSortedAccounts() {
-        List<Account> sortedAccounts = accountService.getSortedAccounts(accounts);
+        List<Account> sortedAccounts = accountServiceForFun.getSortedAccounts(accounts);
         assertNotNull(sortedAccounts);
         assertEquals("Clark", sortedAccounts.get(0).getLastName());
     }
 
     @Test
     void testGetOldestAccount() {
-        Account oldestAccount = accountService.getOldestAccount(accounts);
+        Account oldestAccount = accountServiceForFun.getOldestAccount(accounts);
         assertEquals("John", oldestAccount.getFirstName());
     }
 
     @Test
     void testGroupByBirthYearAndAverageBalance() {
-        Map<Integer, Double> result = accountService.groupByBirthYearAndAverageBalance(accounts);
+        Map<Integer, Double> result = accountServiceForFun.groupByBirthYearAndAverageBalance(accounts);
         assertNotNull(result);
         assertEquals(4, result.size());
     }
 
     @Test
     void testGetAccountWithLongestLastName() {
-        Account accountWithLongestLastName = accountService.getAccountWithLongestLastName(accounts);
+        Account accountWithLongestLastName = accountServiceForFun.getAccountWithLongestLastName(accounts);
         assertEquals("Johnson", accountWithLongestLastName.getLastName());
     }
 
     private static List<Account> createTestAccounts() {
         return Arrays.asList(
-                new Account("John", "Doe", "USA", LocalDate.of(1990, Month.JANUARY, 12), 16000d, Gender.MALE),
-                new Account("Alice", "Johnson", "Canada", LocalDate.of(1995, Month.MARCH, 5), 18000d, Gender.FEMALE),
-                new Account("Bob", "Smith", "UK", LocalDate.of(2000, Month.AUGUST, 22), 13000d, Gender.MALE),
-                new Account("Emily", "Clark", "Germany", LocalDate.of(1992, Month.JULY, 15), 12000d, Gender.FEMALE)
+                new Account(1,"John", "Doe", "USA", LocalDate.of(1990, Month.JANUARY, 12), 16000d, Gender.MALE),
+                new Account(2, "Alice", "Johnson", "Canada", LocalDate.of(1995, Month.MARCH, 5), 18000d, Gender.FEMALE),
+                new Account(3,"Bob", "Smith", "UK", LocalDate.of(2000, Month.AUGUST, 22), 13000d, Gender.MALE),
+                new Account(4,"Emily", "Clark", "Germany", LocalDate.of(1992, Month.JULY, 15), 12000d, Gender.FEMALE)
         );
     }
 }
