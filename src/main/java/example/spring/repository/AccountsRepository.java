a8c380ad0ef64843ac4c7c539cd3dc0d9aee8084
Iliailineya <ilavituk011@gmail.com>
2024-02-13 02:54:09 +0200

    add session based repo

--- src/main/java/example/spring/repository/AccountsRepository.java
+++ src/main/java/example/spring/repository/AccountsRepository.java
@@ -1,23 +1,98 @@
 package example.spring.repository;
 
+import example.spring.exception.AccountNotFoundException;
 import example.spring.model.Account;
-import org.springframework.data.repository.CrudRepository;
+import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface AccountsRepository extends CrudRepository<Account, Long> {
-    @Override
-    <S extends Account> S save(S account);
+public class AccountsRepository {
+    private SessionFactory sessionFactory;
 
-    @Override
-    List<Account> findAll();
+    public AccountsRepository(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
 
-    @Override
-    Optional<Account> findById(Long id);
+    public Account save(Account account) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        return currentSession.merge(account);
+    }
 
-    @Override
-    void deleteById(Long id);
+    @SuppressWarnings("unchecked")
+    public List<Account> findAll() {
+        Session session = sessionFactory.getCurrentSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<Account> cq = cb.createQuery(Account.class);
+        Root<Account> root = cq.from(Account.class);
+        cq.select(root);
+        Query query = session.createQuery(cq);
+        return query.getResultList();
+    }
+
+    public Optional<Account> findById(Long id) {
+        try {
+            Session currentSession = sessionFactory.getCurrentSession();
+            Account account = currentSession.get(Account.class, id);
+            return Optional.ofNullable(account);
+        } catch (RuntimeException e) {
+            throw new AccountNotFoundException("Account not found");
+        }
+    }
+
+    public void updateAccount(Account account) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        currentSession.update(account);
+    }
+
+    public void deleteById(Long id) {
+        Session session = sessionFactory.getCurrentSession();
+        Account account = session.byId(Account.class).load(id);
+        session.remove(account);
+    }
+
+    public boolean existsById(Long id) {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Account account = currentSession.get(Account.class, id);
+        return account != null;
+    }
 }
+//    private final List<Account> accounts = new ArrayList<>();
+//
+//    public List<Account> findAll() {
+//        return accounts;
+//    }
+//
+//    public Optional<Account> findById(long id) {
+//        Optional<Account> account = accounts.stream()
+//                .filter(a -> a.getId() == id)
+//                .findFirst();
+//        if (account.isEmpty()) {
+//            throw new AccountNotFoundException("Account not found");
+//        }
+//        return account;
+//    }
+//
+//    public Account updateById(long id, Account accountToUpdate) {
+//        Optional<Account> possibleAccount = findById(id);
+//        return possibleAccount.map(account -> {
+//            account.setFirstName(accountToUpdate.getFirstName());
+//            account.setLastName(accountToUpdate.getLastName());
+//            account.setCountry(accountToUpdate.getCountry());
+//            account.setBirthday(accountToUpdate.getBirthday());
+//            account.setGender(accountToUpdate.getGender());
+//            return account;
+//        }).orElse(null);
+//    }
+//
+//    public void deleteById(long id) {
+//        Optional<Account> account = findById(id);
+//        account.ifPresent(accounts::remove);
+//    }
--- src/main/java/example/spring/service/AccountService.java
+++ src/main/java/example/spring/service/AccountService.java
@@ -3,6 +3,7 @@ package example.spring.service;
 import example.spring.exception.AccountNotFoundException;
 import example.spring.model.Account;
 import example.spring.repository.AccountsRepository;
+import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,32 +12,36 @@ import java.util.List;
 @Service
 @AllArgsConstructor
 public class AccountService {
-
     private final AccountsRepository repository;
 
+    @Transactional
     public Account createAccount(Account account) {
         return repository.save(account);
     }
 
+    @Transactional
     public Account getAccountById(long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new AccountNotFoundException("Account with id " + id + " not found"));
     }
 
+    @Transactional
     public List<Account> getAllAccounts() {
         return repository.findAll();
     }
 
+    @Transactional
     public Account updateAccount(long id, Account account) {
         if (repository.existsById(id)) {
             account.setId(id);
-            return repository.save(account);
+            repository.updateAccount(account);
+            return account;
         } else {
             throw new AccountNotFoundException("Account with id " + id + " not found");
         }
     }
 
-
+    @Transactional
     public void deleteAccountById(long id) {
         if (repository.existsById(id)) {
             repository.deleteById(id);
