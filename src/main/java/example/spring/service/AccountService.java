7c451746bc686c0d4034252fef757db0c13cd1ea
Iliailineya <ilavituk011@gmail.com>
2024-02-14 13:53:39 +0200

    delete @Transactional

--- src/main/java/example/spring/service/AccountService.java
+++ src/main/java/example/spring/service/AccountService.java
@@ -3,7 +3,6 @@ package example.spring.service;
 import example.spring.exception.AccountNotFoundException;
 import example.spring.model.Account;
 import example.spring.repository.AccountsRepository;
-import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,23 +13,19 @@ import java.util.List;
 public class AccountService {
     private final AccountsRepository repository;
 
-    @Transactional
     public Account createAccount(Account account) {
         return repository.save(account);
     }
 
-    @Transactional
     public Account getAccountById(long id) {
         return repository.findById(id)
                 .orElseThrow(() -> new AccountNotFoundException("Account with id " + id + " not found"));
     }
 
-    @Transactional
     public List<Account> getAllAccounts() {
         return repository.findAll();
     }
 
-    @Transactional
     public Account updateAccount(long id, Account account) {
         if (repository.existsById(id)) {
             account.setId(id);
@@ -41,7 +36,6 @@ public class AccountService {
         }
     }
 
-    @Transactional
     public void deleteAccountById(long id) {
         if (repository.existsById(id)) {
             repository.deleteById(id);
