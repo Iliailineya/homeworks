9290acd68f00b59cead08d9c519126049ce968e7
Iliailineya <ilavituk011@gmail.com>
2024-02-22 17:19:57 +0200

    Create annotation @RandomValue

---
+++ TestBean/src/main/java/org/example/annotation/RandomValue.java
@@ -0,0 +1,13 @@
+package org.example.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RandomValue {
+    double min() default 0.0;
+    double max() default 1.0;
+}
