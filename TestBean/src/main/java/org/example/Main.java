618177838c95991c889b65eb0a8482a94f7231ea
Iliailineya <ilavituk011@gmail.com>
2024-02-22 17:23:43 +0200

    Create TestBeanPostProcessor and check how it works

--- TestBean/src/main/java/org/example/Main.java
+++ TestBean/src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.example.bean.TestBean;
+import org.example.configuration.TestBeanConfiguration;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(TestBeanConfiguration.class)) {
+            TestBean testBean = context.getBean(TestBean.class);
+            System.out.println("Random value: " + testBean.getValue());
+        }
     }
 }
