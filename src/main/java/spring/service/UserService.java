10af6d927ceb81a377fe120383b8975e6e98d364
Iliailineya <ilavituk011@gmail.com>
2024-02-28 16:36:54 +0200

    add service for new entities

---
+++ src/main/java/spring/service/UserService.java
@@ -0,0 +1,47 @@
+package spring.service;
+
+import spring.exception.UserNotFoundException;
+import spring.model.User;
+import spring.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    public User createUser(User user) {
+        return repository.save(user);
+    }
+
+    public User getUserById(long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException("User with id " + id + " not found"));
+    }
+
+    public List<User> getAllUsers() {
+        return repository.findAll();
+    }
+
+    public User updateUser(long id, User user) {
+        if (repository.existsById(id)) {
+            user.setId(id);
+            repository.save(user);
+            return user;
+        } else {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
+    }
+
+    public void deleteUserById(long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            throw new UserNotFoundException("User with id " + id + " not found");
+        }
+    }
+}
