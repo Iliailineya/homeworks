c8a31ab73570b8d4296187d3bc0cb2ab2b365b87
Iliailineya <ilavituk011@gmail.com>
2024-04-05 12:03:37 +0300

    refactoring

--- src/main/java/spring/HomeworksApplication.java
+++ src/main/java/spring/HomeworksApplication.java
@@ -3,9 +3,11 @@ package spring;
 import org.springframework.boot.Banner;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class HomeworksApplication {
 
     public static void main(String[] args) {
