82b1a9c0b027e45d979862a127d2046dc47f33b1
Iliailineya <ilavituk011@gmail.com>
2024-04-05 15:53:18 +0300

    create module rest-api, moved corresponding context into it

---
+++ rest_api/src/main/java/spring/security/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+//package spring.security.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationProvider;
+//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import spring.security.filter.JwtAuthorizationFilter;
+//@SuppressWarnings("unused")
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    private final UserDetailsService userDetailsService;
+//    private final JwtAuthorizationFilter authorizationFilter;
+//
+//    public SecurityConfig(UserDetailsService userDetailsService, JwtAuthorizationFilter authorizationFilter) {
+//        this.userDetailsService = userDetailsService;
+//        this.authorizationFilter = authorizationFilter;
+//    }
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        httpSecurity
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .authorizeHttpRequests(request -> request
+//                        .requestMatchers("/api/**").hasAnyAuthority("CLIENT", "ADMIN")
+//                        .requestMatchers("/auth/signin", "/auth/signup").permitAll()
+//                        .anyRequest().authenticated())
+//                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
+//        return httpSecurity.build();
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder(10);
+//    }
+//
+//    @Bean
+//    public AuthenticationProvider daoAuthProvider() {
+//        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+//        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+//        daoAuthenticationProvider.setUserDetailsService(userDetailsService);
+//        return daoAuthenticationProvider;
+//    }
+//}
